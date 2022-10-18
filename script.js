c5016a3f3d6ae196bc1bd7f5b4e28d5c511d1f0f
spooketti <115603886+spooketti@users.noreply.github.com>
2022-10-18 22:42:57 +0000

    Add a commit message

--- script.js
+++ script.js
@@ -19,9 +19,16 @@ document.addEventListener('keydown', function(e)
     pos++;
     if(pos == 10)
     {
-      alert("WOW")
-      pos = 0;
-    }
+      
+      function fun() {var a="-webkit-",b='transform:rotate(1turn);',c='transition:4s;';document.head.innerHTML+='<style>body{'+a+b+a+c+b+c}
+
+  fun();
+        pos = 0;
+      
+        }
+     
+      
+    
   }
 else
   {
